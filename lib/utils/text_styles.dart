bc0be1a462515150f48ef3fb477cf7a752d217b8
jusylopes <jusylopes@outlook.com>
2022-11-26 21:49:41 -0300

    refactor character text style

--- lib/utils/text_styles.dart
+++ lib/utils/text_styles.dart
@@ -1,6 +1,14 @@
-abstract class CharacterTextStyles{
+import 'package:flutter/material.dart';
 
+abstract class CharacterTextStyles {
+  TextStyle characterName = const TextStyle(
+    fontSize: 20.0,
+    fontWeight: FontWeight.bold,
+    color: Colors.white,
+  );
 
-
-  
+  TextStyle characterStatus = const TextStyle(
+    fontSize: 12.0,
+    color: Colors.white,
+  );
 }
